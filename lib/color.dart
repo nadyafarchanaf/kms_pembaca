ccb163fe41aff4134ba7fb4ce91a615c177cecce
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2020-04-03 16:37:30 +0700

    inisiasi warna

---
+++ lib/color.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color Grey1 = Color.fromRGBO(51, 51, 51, 1);
+  static const Color Grey2 = Color.fromRGBO(13, 27, 16, 1);
+  static const Color Grey3 = Color.fromRGBO(189, 189, 189, 1);
+  static const Color Grey4 = Color.fromRGBO(224, 224, 224, 1);
+
+  static const Color White1 = Color.fromRGBO(252, 252, 252, 1);
+  static const Color White2 = Color.fromRGBO(241, 235, 227, 1);
+
+  static const Color Red = Color.fromRGBO(199, 91, 57, 1);
+
+  static const Color Orange = Color.fromRGBO(223, 158, 87, 1);
+
+  static const Color Brown1 = Color.fromRGBO(205, 168, 119, 1);
+  static const Color Brown2 = Color.fromRGBO(167, 107, 65, 1);
+
+  static const Color Green1 = Color.fromRGBO(44, 133, 78, 1);
+  static const Color Green2 = Color.fromRGBO(62, 91, 69, 1);
+  static const Color Green3 = Color.fromRGBO(100, 147, 85, 1);
+  static const Color Green4 = Color.fromRGBO(148, 175, 164, 1);
+  static const Color Green5 = Color.fromRGBO(106, 133, 111, 1);
+  static const Color Green6 = Color.fromRGBO(109, 169, 153, 1);
+  static const Color Green7 = Color.fromRGBO(51, 63, 47, 1);
+}
